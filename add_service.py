a42da04bcae05c181f5f71d3df58b3e517b75351
Samarth Agarwal <agarwalsamarth@hotmail.com>
2020-06-26 17:46:35 -0400

    Added a function in add_service.py to validate URL. Some further work to exception handling and verifying connections to URLs is needed.

--- add_service.py
+++ add_service.py
@@ -2,6 +2,7 @@
 import sys
 import uuid
 import argparse
+import requests
 from service_registry.orm.models import URL
 from service_registry.orm import init_db, get_session
 
@@ -17,12 +18,47 @@ def main(args=None):
     parser.add_argument('url', type=str)
     args = parser.parse_args(args)
 
+    if not validate_url(args.url):
+        print(f"{args.url} validation failed", file=sys.stderr, flush=True)
+        sys.exit(1)
+
     init_db(uri="sqlite:///"+args.database)
     db_session = get_session()
 
     new_url = URL(url=args.url, name=args.name, id=uuid.uuid4())
     db_session.add(new_url)
     db_session.commit()
+
+
+def validate_url(url):
+    """
+    Return True iff <url> is a valid URL address.
+    :param url: URL address to be validated
+    :return: bool
+    """
+    valid = False
+    try:
+        print(repr(url))
+        #url = "https://googwle.com:81"
+        print(url)
+        print(type(url))
+        print(len(url))
+        requests.get(url, timeout=5)
+        valid = True
+    except requests.URLRequired:
+        print("Invalid URL", file=sys.stderr, flush=True)
+    except requests.TooManyRedirects:
+        print("Too many redirects", file=sys.stderr, flush=True)
+    except requests.exceptions.ConnectTimeout:
+        print("Timeout occurred", file=sys.stderr, flush=True)
+    except requests.ConnectionError as e:
+        print("Unable to connect", file=sys.stderr, flush=True)
+        print(e)
+    except requests.RequestException as e:
+        print("Unknown error", file=sys.stderr, flush=True)
+        print(e)
+
+    return valid
 
 
 if __name__ == "__main__":
