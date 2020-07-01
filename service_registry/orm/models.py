a4b58fea775b9482839efd865c131c3190f01be7
Samarth Agarwal <agarwalsamarth@hotmail.com>
2020-07-01 17:29:36 -0400

    Added ORM models.

--- service_registry/orm/models.py
+++ service_registry/orm/models.py
@@ -3,8 +3,8 @@ SQLAlchemy models for the database
 """
 import datetime
 import uuid
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy import UniqueConstraint
+from sqlalchemy import Column, String, DateTime, Boolean
+from sqlalchemy import UniqueConstraint, ForeignKey
 from service_registry.orm.guid import GUID
 from service_registry.orm import Base
 
@@ -18,6 +18,47 @@ class URL(Base):
     name = Column(String(100))
     url = Column(String(100))
     added = Column(DateTime, default=datetime.datetime.utcnow)
+    service = Column(GUID(), ForeignKey('services.id'), default=None)
     __table_args__ = (
-        UniqueConstraint("url"),
+        UniqueConstraint('url'),
     )
+
+
+class Type(Base):
+    """
+    SQLAlchemy class/table representing type of a service
+    """
+    __tablename__ = 'types'
+    id = Column(GUID(), primary_key=True, default=uuid.uuid4)
+    group = Column(String(100))
+    artifact = Column(String(100))
+    version = Column(String(100))
+
+
+class Organization(Base):
+    """
+    SQLAlchemy class/table representing organization of a service
+    """
+    __tablename__ = 'organizations'
+    id = Column(GUID(), primary_key=True, default=uuid.uuid4)
+    name = Column(String(100))
+    url = Column(String(100))
+
+
+class Service(Base):
+    """
+    SQLAlchemy class/table representing a service
+    """
+    __tablename__ = 'services'
+    id = Column(GUID(), primary_key=True, default=uuid.uuid4)
+    name = Column(String(100))
+    type = Column(GUID(), ForeignKey('types.id'), default=None)
+    description = Column(String(100))
+    organization = Column(GUID(), ForeignKey('organizations.id'), default=None)
+    contact_url = Column(String(100))
+    documentation_url = Column(String(100))
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+    environment = Column(String(100))
+    version = Column(String(100))
+    active = Column(Boolean())
