16b9babfab9948da51a495dbef27f8b4516e6537
Samarth Agarwal <agarwalsamarth@hotmail.com>
2020-07-14 15:20:20 -0400

    Added functionality in operations.py to get service info when the URL is not able to be connected to.

--- service_registry/api/operations.py
+++ service_registry/api/operations.py
@@ -4,6 +4,7 @@
 Implement endpoints of model service
 """
 import sys
+import uuid
 from urllib.parse import urljoin
 import requests
 from service_registry import orm
@@ -11,7 +12,8 @@ from service_registry.orm import models
 from service_registry.api.logging import apilog
 from service_registry.api.models import Error, ServiceType
 from service_registry.api.models import Service, ExternalService
-from service_registry.orm.models import URL
+from service_registry.orm.models import URL, Service as ORM_Service, Type, Organization
+from tornado.options import options
 
 SERVICE_NAME = "CINECA Service Registry"
 THIS_SERVICE_TYPE = ServiceType(group='org.ga4gh', artifact='service-registry', version='1.0.0')
@@ -22,10 +24,21 @@ THIS_SERVICE = Service(id='this.service',
                        organization={'name': "CINECA", 'url': 'www.cineca-project.eu'},
                        version='1.0.0')
 
-def get_service_info(url):
+
+def get_service_info_and_active_status(url):
     """
-    Fetch service info from url
+    Fetch service info and active status of url
     """
+    db_session = orm.get_session()
+    active = True
+
+    try:
+        url_orm = db_session.query(URL).filter(URL.url == url).scalar()
+    except orm.ORMException as e:
+        print("Error connecting to database: " + str(e), file=sys.stderr, flush=True)
+        return None, active
+
+    service_orm = url_orm.service
     service_info_url = urljoin(f"{url}/", "service-info")
     print(f"[{SERVICE_NAME}] Contacting {service_info_url}", flush=True)
     try:
@@ -33,12 +46,40 @@ def get_service_info(url):
         if r.status_code != 200:
             print(f"[{SERVICE_NAME}] Non-200 status code on {service_info_url}: {r.status_code}", file=sys.stderr,
                   flush=True)
-            return None
+            service_data = get_service_data_from_db(service_orm)
+            active = False
+        else:
+            service_data = r.json()
     except requests.exceptions.Timeout:
         print(f"[{SERVICE_NAME}] Encountered timeout with {service_info_url}", file=sys.stderr, flush=True)
-        return None
+        service_data = get_service_data_from_db(service_orm)
+        active = False
 
-    service_data = r.json()
+    if not service_data:
+        return None, active
+
+    # add the service to the database if it is not already in the database
+    if not service_orm:
+        service_type = Type(id=uuid.uuid4(), group=service_data['type']['group'],
+                            artifact=service_data['type']['artifact'], version=service_data['type']['version'])
+        organization = Organization(id=uuid.uuid4(), name=service_data['organization']['name'],
+                                    url=service_data['organization']['url'])
+        service = ORM_Service(id=uuid.uuid4(), name=service_data['name'], description=service_data['description'],
+                              contact_url=service_data['contactUrl'],
+                              documentation_url=service_data['documentationUrl'],
+                              created_at=service_data['createdAt'], updated_at=service_data['updatedAt'],
+                              environment=service_data['environment'], version=service_data['version'])
+        service.type = service_type
+        service.organization = organization
+        url_orm.service = service
+
+        try:
+            db_session.add(service)
+            db_session.commit()
+        except orm.ORMException as e:
+            print("Error writing to database: " + str(e), file=sys.stderr, flush=True)
+            db_session.rollback()
+
     if "cohorts" not in service_data:
         cohort_url = urljoin(f"{url}/", "cohorts")
         cr = requests.get(cohort_url, timeout=1)
@@ -47,7 +88,39 @@ def get_service_info(url):
         else:
             service_data.update["cohorts"] = cr.json()
 
-    return Service(**service_data)
+    return Service(**service_data), active
+
+
+def get_service_data_from_db(service_orm):
+    """
+    Fetch service data from service_orm
+    """
+    if not service_orm:
+        return None
+
+    type_orm = service_orm.type
+    organization_orm = service_orm.organization
+
+    type_dict = orm.dump(type_orm, nonulls=False)
+    type_dict.pop('id', None)
+
+    org_dict = orm.dump(organization_orm, nonulls=False)
+    org_dict.pop('id', None)
+
+    service_dict = orm.dump(service_orm, nonulls=False)
+    service_dict.pop('type_id', None)
+    service_dict.pop('organization_id', None)
+    service_dict['id'] = str(service_dict['id'])
+    service_dict['type'] = type_dict
+    service_dict['organization'] = org_dict
+
+    pothole_to_camel_case = {'contact_url': 'contactUrl', 'documentation_url': 'documentationUrl',
+                             'created_at': 'createdAt', 'updated_at': 'updatedAt'}
+
+    for key in pothole_to_camel_case:
+        service_dict[pothole_to_camel_case[key]] = service_dict.pop(key)
+
+    return service_dict
 
 
 @apilog
@@ -63,7 +136,7 @@ def list_services():
 
     external_services = []
     for url in urls:
-        service = get_service_info(url.url)
+        service, active = get_service_info_and_active_status(url.url)
         if not service:
             continue
         # overwrite ID and name with local values
@@ -71,6 +144,9 @@ def list_services():
         service.name = url.name
         service_as_dict = orm.dump(service)
         service_as_dict['url'] = url.url
+
+        if options.show_active_status:
+            service_as_dict['active'] = active
 
         external_services.append(ExternalService(**service_as_dict))
 
@@ -91,7 +167,7 @@ def get_one_service(serviceId):
     if not q:
         return Error(title="No such service found", detail="No result for service "+str(serviceId), status=404), 404
 
-    service = get_service_info(q.url)
+    service, active = get_service_info_and_active_status(q.url)
 
     if not service:
         return Error(title="Service not available", detail="Could not connect to service "+str(serviceId), status=404), 404
@@ -101,6 +177,10 @@ def get_one_service(serviceId):
     service.name = q.name
     service_as_dict = orm.dump(service)
     service_as_dict['url'] = q.url
+
+    if options.show_active_status:
+        service_as_dict['active'] = active
+
     external_service = ExternalService(**service_as_dict)
     return orm.dump(external_service), 200, {'Access-Control-Allow-Origin':'*'}
 
@@ -126,7 +206,7 @@ def list_service_types():
 
     service_types = []
     for url in urls:
-        service = get_service_info(url.url)
+        service, _ = get_service_info_and_active_status(url.url)
         if not service:
             continue
         service_type = ServiceType(**service.type)
